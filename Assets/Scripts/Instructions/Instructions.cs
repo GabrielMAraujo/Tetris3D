5cfc6e525e5d7930926f9f7b3867da1edc5d980a
Gabriel Mota Araújo <gabriel.mota39@gmail.com>
2021-02-03 13:44:13 -0300

    feat(main_menu): basic main menu and instructions

---
+++ Assets/Scripts/Instructions/Instructions.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Instructions : MonoBehaviour
+{
+    public void CloseInstructions()
+    {
+        //Removes Instructions from scene stack
+        SceneManager.UnloadSceneAsync("Instructions");
+    }
+}
---
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void OpenInstructions()
+    {
+        //Adds Instructions to scene stack
+        SceneManager.LoadScene("Instructions", LoadSceneMode.Additive);
+    }
+
+    public void OpenGameplay()
+    {
+        SceneManager.LoadScene("Gameplay", LoadSceneMode.Single);
+    }
+}
